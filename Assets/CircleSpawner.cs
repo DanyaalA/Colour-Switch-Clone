37ceadf2d1b0f62dfbb2875a099d0e2edd7f10a8
DanyaalA <Danny13653@gmail.com>
2021-01-17 22:58:21 +0000

    Colour Switch

---
+++ Assets/CircleSpawner.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CircleSpawner : MonoBehaviour
+{
+    public GameObject circlePrefab;
+    public GameObject changerPrefab;
+    public GameObject coinPrefab;
+    public float respawnTime = 1f;
+    private float currentY = 3.6f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    int prevScore = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player ply = FindObjectOfType<Player>();
+        if (prevScore != ply.Score)
+        {
+            prevScore = ply.Score;
+            SpawnCircle();
+        }
+    }
+
+    public void SpawnCircle()
+    {
+
+        GameObject changer = Instantiate(changerPrefab);
+        GameObject circle = Instantiate(circlePrefab);
+        GameObject coin = Instantiate(coinPrefab);
+
+        currentY += 4.5f;
+
+        changer.transform.position = new Vector3(changer.transform.position.x, currentY, changer.transform.position.z);
+
+        currentY += 3.9f;
+
+        circle.transform.position = new Vector3(circle.transform.position.x, currentY, circle.transform.position.z);
+
+        coin.transform.position = new Vector3(coin.transform.position.x, currentY, coin.transform.position.z);
+
+
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
+public class Player : MonoBehaviour
+{
+    public float jumpForce = 10f;
+
+    public Rigidbody2D rb;
+
+    public string currentCol;
+    public SpriteRenderer sr;
+    public int Score = 0;
+
+    public Color colCyan;
+    public Color colYellow;
+    public Color colMagenta;
+    public Color colPink;
+
+    private void Start()
+    {
+        SetRandomColour();
+    }
+
+    void OnBecameInvisible()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log(collision.tag);
+
+        if (collision.tag == "Coin")
+        {
+            Score++;
+            Debug.Log("Coin Hit");
+            Destroy(collision.gameObject);
+            
+            return;
+        }
+
+        if (collision.tag == "ColourChanger")
+        {
+            SetRandomColour();
+            Destroy(collision.gameObject);
+            return;
+        }
+
+        if (collision.tag != currentCol && collision.tag != "Coin")
+        {
+            Debug.Log("GAME OVER");
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            
+        }
+
+
+    }
+
+    void SetRandomColour()
+    {
+        int index = UnityEngine.Random.Range(0, 4);
+
+        switch (index)
+        {
+            case 0:
+                //if (SameColour("Cyan")) SetRandomColour();
+                currentCol = "Cyan";
+                sr.color = colCyan;
+                break;
+            case 1:
+                //if (SameColour("Yellow")) SetRandomColour();
+                currentCol = "Yellow";
+                sr.color = colYellow;
+                break;
+            case 2:
+                //if (SameColour("Magenta")) SetRandomColour();
+                currentCol = "Magenta";
+                sr.color = colMagenta;
+                break;
+            case 3:
+                //if (SameColour("Pink")) SetRandomColour();
+                currentCol = "Pink";
+                sr.color = colPink;
+                break;
+        }
+    }
+
+    private bool SameColour(string newCol)
+    {
+        if (newCol == currentCol) return true;
+
+        return false;
+    }
+}
